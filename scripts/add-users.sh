fc75167b08dfe708211ab10a3256cbaeed242f68
MikeKelley138 <mkelley@medianewsgroup.com>
2024-06-07 15:38:21 -0600

    Update add-users.sh

--- scripts/add-users.sh
+++ scripts/add-users.sh
@@ -2,24 +2,27 @@
 
 CSV_FILE="users.csv"
 
+# Check if the CSV file exists
 if [[ ! -f "$CSV_FILE" ]]; then
   echo "CSV file not found"
   exit 1
 fi
 
+# Read the CSV file line by line
 while IFS=, read -r role first_name last_name email; do
-
+  # Skip the header line
   if [[ "$role" == "role" ]]; then
     continue
   fi
 
-  # Generate username - same as email
+  # Generate username (same as email)
   username="$email"
 
-  command="vip @385.develop -- wp user create $username $email --role=$role --first_name=$first_name --last_name=$last_name"
+  # Create the WP CLI command
+  wp_command="wp user create \"$username\" \"$email\" --role=\"$role\" --first_name=\"$first_name\" --last_name=\"$last_name\""
 
-  echo "running command: $command"
-
-  eval "$command"
+  # Run the WP CLI command
+  echo "Running command: $wp_command"
+  eval "$wp_command"
 
 done < "$CSV_FILE"
