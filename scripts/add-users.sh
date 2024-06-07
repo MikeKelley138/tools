b017ec605c204389d51feb9cdf2b6af240553201
MikeKelley138 <mkelley@medianewsgroup.com>
2024-06-07 15:13:48 -0600

    Update add-users.sh

--- scripts/add-users.sh
+++ scripts/add-users.sh
@@ -2,25 +2,24 @@
 
 CSV_FILE="users.csv"
 
-if [[ ! -f "$CSV_FILE"]]; then
+if [[ ! -f "$CSV_FILE" ]]; then
   echo "CSV file not found"
   exit 1
 fi
 
-while IFS=, read -r role first_name last_name email
-do
+while IFS=, read -r role first_name last_name email; do
 
-  if[[ "$role" == "role"]]; then
+  if [[ "$role" == "role" ]]; then
     continue
   fi
 
-  #generate username - same as email
-  username=$email
+  # Generate username - same as email
+  username="$email"
 
   command="vip @385.develop -- wp user create $username $email --role=$role --first_name=$first_name --last_name=$last_name"
 
   echo "running command: $command"
 
-  eval $command
+  eval "$command"
 
 done < "$CSV_FILE"
